1df1cf298c2beea59c12335258e7dd39a17c4792
joão vítor <102699222+jvittor@users.noreply.github.com>
2024-09-26 14:33:39 +0000

    fix(MapBiomesUpdates): change url backend request

--- src/lib/components/Home/MapBiomesUpdates.tsx
+++ src/lib/components/Home/MapBiomesUpdates.tsx
@@ -26,7 +26,7 @@ const MapBiomes: React.FC = () => {
     setLoading(true);
     try {
       const response = await axios.get(
-        'https://psychic-parakeet-q5grgjr9vwg39gw9-3333.app.github.dev/scrape-news'
+        'https://backend-rclimaticas-2.onrender.com/scrape-news'
       );
       console.log(response.data);
       if (Array.isArray(response.data)) {
