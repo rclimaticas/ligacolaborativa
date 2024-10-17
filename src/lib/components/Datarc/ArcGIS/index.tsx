a45d482ba22fb2e68a300301da9c837d8c41b753
joão vítor <102699222+jvittor@users.noreply.github.com>
2024-10-17 11:37:47 +0000

    fix(ArcGIS): bottom size

--- src/lib/components/Datarc/ArcGIS/index.tsx
+++ src/lib/components/Datarc/ArcGIS/index.tsx
@@ -3,7 +3,7 @@ import Title from '@/lib/components/Datarc/ArcGIS/Title';
 
 export default function ArcGIS() {
   return (
-    <div className="relative bottom-[400px] flex grid h-screen flex-col items-center justify-center gap-5 p-5 md:p-20">
+    <div className="relative bottom-[240px] flex grid h-screen flex-col items-center justify-center gap-5 p-5 md:p-20 xl:bottom-[400px]">
       <Title />
       <Doodle />
     </div>
