50d51021ac8cb76cca41a3ae6e9e6c1ff0f35eb4
joão vítor <102699222+jvittor@users.noreply.github.com>
2024-11-05 13:07:36 +0000

    refactor(Contribution): modal structure contribution

--- src/lib/pages/datarc/index.tsx
+++ src/lib/pages/datarc/index.tsx
@@ -4,6 +4,7 @@ import type { NextPage } from 'next/types';
 
 import ArcGIS from '@/lib/components/Datarc/ArcGIS/index';
 import CarouselAndContribution from '@/lib/components/Datarc/CarouselAndContribution/index';
+import Contribution from '@/lib/components/Datarc/Contribution/index';
 import Hero from '@/lib/components/Datarc/Hero/index';
 import Maps from '@/lib/components/Datarc/Maps/index';
 import NewsletterAndTrustedBy from '@/lib/components/Datarc/NewsletterAndTrustedBy/index';
@@ -14,6 +15,7 @@ const DataRC: NextPage = () => {
     <div className="h-[4600px] xl:h-[4600px]">
       <Hero />
       <CarouselAndContribution />
+      <Contribution />
       <Spotlight />
       <ArcGIS />
       <Maps />
