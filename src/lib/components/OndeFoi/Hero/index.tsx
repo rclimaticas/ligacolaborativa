3f23d8eba1d64be7873a0c902879971c0f1c6c6c
joão vítor <102699222+jvittor@users.noreply.github.com>
2024-11-29 14:13:32 +0000

    fix(Index): eslint

--- src/lib/components/OndeFoi/Hero/index.tsx
+++ src/lib/components/OndeFoi/Hero/index.tsx
@@ -4,7 +4,7 @@ import Title from '@/lib/components/OndeFoi/Hero/Title';
 
 export default function Hero() {
   return (
-    <div className="mt-12 flex h-full w-full flex-col items-center justify-center gap-10 bg-gradient p-4 md:gap-3 lg:flex-row lg:p-32">
+    <div className="bg-gradient mt-12 flex h-full w-full flex-col items-center justify-center gap-10 p-4 md:gap-3 lg:flex-row lg:p-32">
       <Title />
       <Doodle />
     </div>
