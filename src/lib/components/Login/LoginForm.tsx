365022d4eb5975724b1a141ecbeabbff6d95ebd3
Projeto Resiliências Climáticas <162454027+rclimaticas@users.noreply.github.com>
2025-01-13 14:38:28 +0000

    refactor(Component): login and register backend integration

--- src/lib/components/Login/LoginForm.tsx
+++ src/lib/components/Login/LoginForm.tsx
@@ -1,14 +1,16 @@
+/* eslint-disable no-alert */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-// src/components/LoginForm.tsx
-/* eslint-disable jsx-a11y/label-has-associated-control */
-
-'use client';
-
+import CircularProgress from '@mui/material/CircularProgress';
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import { GoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import type React from 'react';
+import { useState } from 'react';
 
 const CustomTextField = styled(TextField)({
   '& input:-webkit-autofill': {
@@ -17,12 +19,55 @@ const CustomTextField = styled(TextField)({
   },
 });
 
+interface LoginData {
+  email: string;
+  password: string;
+}
+
 export default function LoginForm() {
+  const [loginData, setLoginData] = useState<LoginData>({
+    email: '',
+    password: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const handleLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        'https://backend-production-2453.up.railway.app/login',
+        loginData,
+        {
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+      alert(`Login bem-sucedido! Token: ${response.data.token}`);
+      router.push('/');
+    } catch (err: any) {
+      setError(err.response?.data?.message || 'Ocorreu um erro inesperado.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-center font-roboto">
         <div className="min-w-4xl grid w-[500px] rounded-3xl bg-white shadow-none lg:shadow-2xl">
-          <form className="flex w-full flex-col items-center justify-center gap-6 p-10 font-roboto text-black-200 lg:gap-10">
+          <form
+            className="flex w-full flex-col items-center justify-center gap-6 p-10 font-roboto text-black-200 lg:gap-10"
+            onSubmit={handleSubmit}
+          >
             <div className="flex w-full justify-center text-2xl">
               <Image
                 alt="Logo"
@@ -49,16 +94,22 @@ export default function LoginForm() {
             <CustomTextField
               required
               id="email"
+              name="email"
               label="Email"
               variant="outlined"
               fullWidth
+              onChange={handleLogin}
+              value={loginData.email}
             />
             <CustomTextField
               required
               id="password"
+              name="password"
               label="Senha"
               variant="outlined"
               fullWidth
+              onChange={handleLogin}
+              value={loginData.password}
             />
             <a
               href="/register"
@@ -68,10 +119,11 @@ export default function LoginForm() {
             </a>
             <button
               type="submit"
-              className="w-full rounded-lg border-2 border-black-300 bg-orange p-2 text-xl font-semibold text-black-300 shadow-[rgba(0,0,15,0.5)_-3px_5px_4px_0px]"
+              className="flex w-full items-center justify-center rounded-lg border-2 border-black-300 bg-orange p-2 text-xl font-semibold text-black-300 shadow-[rgba(0,0,15,0.5)_-3px_5px_4px_0px]"
             >
-              ENTRAR
+              {loading ? <CircularProgress size="30px" /> : 'ENTRAR'}
             </button>
+            {error && <p className="text-red-500">{error}</p>}
           </form>
         </div>
       </div>
--- src/lib/components/Register/RegisterForm.tsx
+++ src/lib/components/Register/RegisterForm.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-alert */
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 // src/components/LoginForm.tsx
@@ -5,22 +7,83 @@
 
 'use client';
 
+import CircularProgress from '@mui/material/CircularProgress';
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import { GoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import type React from 'react';
 
 const CustomTextField = styled(TextField)({
   '& input:-webkit-autofill': {
-    WebkitBoxShadow: '0 0 0 1000px white inset', // Altere "white" para a cor desejada
-    WebkitTextFillColor: 'black', // Altere "black" para a cor do texto desejada
+    WebkitBoxShadow: '0 0 0 1000px white inset',
+    WebkitTextFillColor: 'black',
   },
 });
 
+interface RegisterData {
+  username: string;
+  email: string;
+  password: string;
+}
+
 export default function RegisterForm() {
+  const [registerData, setRegisterData] = useState<RegisterData>({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const handleLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setRegisterData({ ...registerData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+    setLoading(true);
+
+    if (
+      !registerData.username ||
+      !registerData.email ||
+      !registerData.password
+    ) {
+      setError('Por favor, preencha todos os campos.');
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        'https://backend-production-2453.up.railway.app/register',
+        registerData,
+        {
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+      alert(`Registro bem-sucedido`);
+      console.log(response.data);
+      router.push('/login');
+    } catch (err: any) {
+      setError(err.response?.data?.message || 'Ocorreu um erro inesperado.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex w-full items-center justify-center font-roboto">
       <div className="min-w-4xl grid w-[500px] rounded-3xl bg-white shadow-none lg:shadow-2xl">
-        <form className="flex w-full flex-col items-center justify-center gap-3 p-10 font-roboto text-black-200 lg:gap-6">
+        <form
+          onSubmit={handleSubmit}
+          className="flex w-full flex-col items-center justify-center gap-3 p-10 font-roboto text-black-200 lg:gap-6"
+        >
           <div className="flex w-full justify-center text-2xl">
             Se registre para contribuir.
           </div>
@@ -41,24 +104,34 @@ export default function RegisterForm() {
           </div>
           <CustomTextField
             required
-            id="name"
+            id="username"
+            name="username"
             label="Nome"
             variant="outlined"
             fullWidth
+            value={registerData.username}
+            onChange={handleLogin}
           />
           <CustomTextField
             required
             id="email"
+            name="email"
             label="Email"
             variant="outlined"
             fullWidth
+            value={registerData.email}
+            onChange={handleLogin}
           />
           <CustomTextField
             required
             id="password"
+            name="password"
             label="Senha"
             variant="outlined"
+            type="password"
             fullWidth
+            value={registerData.password}
+            onChange={handleLogin}
           />
           <a
             href="/login"
@@ -66,12 +139,19 @@ export default function RegisterForm() {
           >
             Já fez seu cadastro? Faça seu login e contribua!{' '}
           </a>
-          <button
+          {/* <button
             type="submit"
             className="w-full rounded-lg border-2 border-black-300 bg-orange p-2 text-xl font-semibold text-black-300 shadow-[rgba(0,0,15,0.5)_-3px_5px_4px_0px]"
           >
             Cadastrar
+          </button> */}
+          <button
+            type="submit"
+            className="flex w-full items-center justify-center rounded-lg border-2 border-black-300 bg-orange p-2 text-xl font-semibold text-black-300 shadow-[rgba(0,0,15,0.5)_-3px_5px_4px_0px]"
+          >
+            {loading ? <CircularProgress size="30px" /> : 'Cadastrar'}
           </button>
+          {error && <p className="text-red-500">{error}</p>}
         </form>
       </div>
     </div>
