2c388f1e13bfe41b912213d277d02517f822afeb
VYT <102699222+jvittor@users.noreply.github.com>
2025-02-28 19:21:23 +0000

    refactor(Impact): back integration modal

--- src/lib/components/User/Impact/Impact.tsx
+++ src/lib/components/User/Impact/Impact.tsx
@@ -1,15 +1,137 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/button-has-type */
+
+'use client';
+
 import ReplaySharpIcon from '@mui/icons-material/ReplaySharp';
 import SearchIcon from '@mui/icons-material/Search';
+import {
+  Modal,
+  Box,
+  Typography,
+  TextField,
+  CircularProgress,
+} from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 import { useState } from 'react';
+import { IoArrowBack } from 'react-icons/io5';
+import { toast } from 'react-toastify';
 
 import Table from '@/lib/components/User/Impact/Table';
+import {
+  removeUserData,
+  updateUserData,
+  getUserData,
+} from '@/services/UserStorage';
+
+interface Impact {
+  id: string;
+  subject: string;
+  locality: string;
+  support: string;
+  affectedCommunity: string[];
+  biomes: string[];
+  situation: string;
+  contribution: string;
+}
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: { md: 560, xs: 400 },
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const CustomTextField = styled(TextField)({
+  '& input:-webkit-autofill': {
+    WebkitBoxShadow: '0 0 0 1000px white inset',
+    WebkitTextFillColor: 'black',
+  },
+});
 
 const ImpactComponent: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [openModal, setOpenModal] = useState<string | null>(null);
+  const [isLoadingButton, setIsLoadingButton] = useState(false);
+  const [subject, setSubject] = useState('');
+  const [locality, setLocality] = useState('');
+  const [support, setSupport] = useState('');
+  const [affectedCommunity, setAffectedCommunity] = useState<string[]>([]);
+  const [biomes, setBiomes] = useState<string[]>([]);
+  const [situation, setSituation] = useState('');
+  const [contribution, setContribution] = useState('');
+
+  const handleUserImpact = async (data: Partial<Impact>) => {
+    setIsLoadingButton(true);
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/user/update`,
+        {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data),
+          credentials: 'include',
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Erro ao atualizar o usuário');
+      }
+
+      const updatedUser = await response.json();
+
+      if (!updatedUser?.id) {
+        throw new Error('Resposta inválida do servidor');
+      }
+
+      console.log('Dados atualizados recebidos:', updatedUser);
+
+      // Remove os dados antigos do IndexedDB
+      await removeUserData(updatedUser.id);
+
+      // Atualiza os dados no IndexedDB
+      await updateUserData(updatedUser.id, updatedUser);
+
+      const freshUserData = await getUserData();
+      console.log('Dados atualizados no IndexedDB:', freshUserData);
+
+      setOpenModal(null);
+
+      toast.success('Dados alterados com sucesso!', {
+        position: 'top-right',
+        autoClose: 2000,
+      });
+      window.location.reload();
+    } catch (error) {
+      toast.error('Erro ao salvar os dados!', {
+        position: 'top-right',
+        autoClose: 2000,
+      });
+      console.error('Erro ao salvar os dados do usuário:', error);
+    } finally {
+      setIsLoadingButton(false);
+    }
+  };
+
+  const handleSaveImpact = () =>
+    handleUserImpact({
+      subject,
+      locality,
+      support,
+      affectedCommunity,
+      biomes,
+      situation,
+      contribution,
+    });
 
   return (
     <div className="flex flex-col space-y-5">
